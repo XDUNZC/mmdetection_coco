8541ee29375c877443f9a0b505f767ff1ea0557b
Shaohon Chen <shaohon_chen@foxmail.com>
2020-03-29 07:51:03 +0000

    set checkpoint in run.py <2>add my.py

--- run.py
+++ run.py
@@ -48,18 +48,22 @@ def get_feature_extractor(config_file='configs/tbtc_fater_rcnn_voc.py',
 # todo 封装时注意一个问题：不要把模型model读进来初始化，检测完丢掉，再读进来，再检测完再丢掉。注意节省时间
 if __name__ == '__main__':
     # demo 检测图片 注意替换文件路径
-    # config_file = 'configs/my.py'  # 替换为指定的configs/xx.py
-    # checkpoint_file = 'work_dirs/retinanet_x101_64x4d_fpn_1x/latest.pth'  # 替换为预训练权重
-    # device = 'cuda:0'  # GPU 卡号
-    # model = get_model(config_file=config_file, checkpoint_file=checkpoint_file, device=device)
-    # img_path = 'demo/demo.jpg'
-    # bboxes = get_result_box(img_path=img_path, model=model, score_thr=0.7) # array，输出格式是(N,5),N个满足条件的框 每个框与5个值，前4个是位置信息，最后一个是概率值 0-1
-    # print(bboxes)
+    config_file = 'configs/my.py'  # 替换为指定的configs/xx.py
+    checkpoint_file = 'checkpoints/retinanet_x101_64x4d_fpn_1x20200322-53c08bb4.pth'  # 替换为预训练权重
+    device = 'cuda:0'  # GPU 卡号
+    model = get_model(config_file=config_file, checkpoint_file=checkpoint_file, device=device)
+    img_path = 'demo/demo.jpg'
+    bboxes = get_result_box(img_path=img_path, model=model, score_thr=0.7) # array，输出格式是(N,5),N个满足条件的框 每个框与5个值，前4个是位置信息，最后一个是概率值 0-1
+    print(bboxes)
 
     # demo 生成feature_extractor
-    img = 'demo/tbtc_test.py'
-    feature_extractor = get_feature_extractor(config_file='configs/tbtc_retinanet_voc.py',
-                          checkpoint_file='checkpoints/retinanet_x101_64x4d_fpn_1x20200322-53c08bb4.pth',
-                          device='cuda:0')
-    feature_maps = feature_extractor(img)
-    print(feature_maps)
+    # img = 'demo/tbtc_test.jpg'
+    # feature_extractor = get_feature_extractor(config_file='configs/tbtc_retinanet_voc.py',
+    #                       checkpoint_file='checkpoints/retinanet_x101_64x4d_fpn_1x20200322-53c08bb4.pth',
+    #                       device='cuda:0')
+    # feature_maps = feature_extractor(img)
+    # print(feature_maps)
+
+
+
+
