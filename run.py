fca978ac8b3dffb74c7da5f8244a57d910d3ca3e
Shaohon_Chen <611699999@qq.com>
2020-03-29 15:13:31 +0800

    finish all code

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from mmdet.apis import init_detector, inference_detector
+from .mmdet.apis import init_detector, inference_detector
 import numpy as np
 import torch.nn as nn
+
 
 def get_model(config_file='configs/my.py',
               checkpoint_file='work_dirs/retinanet_x101_64x4d_fpn_1x/latest.pth',
@@ -27,6 +28,7 @@ def get_result_box(img_path, model, score_thr=0.7):
     inds = np.where(bboxes[:, -1] > score_thr)[0]
     bboxes_over_thr = bboxes[inds]
     return bboxes_over_thr
+
 
 # todo 目前这个只支持onestage模型
 def get_feature_extractor(config_file='configs/tbtc_fater_rcnn_voc.py',
@@ -59,7 +61,7 @@ if __name__ == '__main__':
     # demo 生成feature_extractor
     img = 'demo/tbtc_test.py'
     feature_extractor = get_feature_extractor(config_file='configs/tbtc_retinanet_voc.py',
-                          checkpoint_file='checkpoints/retinanet_x101_64x4d_fpn_1x20200322-53c08bb4.pth',
-                          device='cuda:0')
+                                              checkpoint_file='checkpoints/retinanet_x101_64x4d_fpn_1x20200322-53c08bb4.pth',
+                                              device='cuda:0')
     feature_maps = feature_extractor(img)
     print(feature_maps)
